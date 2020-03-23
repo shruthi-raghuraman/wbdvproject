1cb8edba5879cacda2901a9bf42d9a82160e365b
ffeghali <feghali.fa@husky.neu.edu>
2020-03-23 14:39:44 -0400

    specified more files, small change - f

---
+++ wbdv-sp20-group2-client-react/src/components/SearchBarComponent.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class SearchBarComponent extends Component {
+    render() {
+        return (
+            //TODO decide how to go about submitting to api,
+            // currently using form with action
+            //TODO add CSS and format
+            <div>
+                <h1> RECIPES RADAR </h1>
+                <form className = "search" action="google api placeholder">
+                    <input type ="text"
+                           className= "input"
+                           placeholder= "Search Recipes"/>
+                    <button type = "submit"> Search </button>
+                </form>
+            </div>
+
+
+        );
+    }
+}
+
+
+export default SearchBarComponent;
