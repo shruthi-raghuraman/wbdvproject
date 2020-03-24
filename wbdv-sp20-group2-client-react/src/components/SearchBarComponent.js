40961d74d8ed94eeb4dee845a68631a06c91c4c8
Robert Dragomir <rgdragomir@gmail.com>
2020-03-24 15:47:53 -0400

    First Commit

--- wbdv-sp20-group2-client-react/src/components/SearchBarComponent.js
+++ wbdv-sp20-group2-client-react/src/components/SearchBarComponent.js
@@ -7,18 +7,16 @@ class SearchBarComponent extends Component {
             // currently using form with action
             //TODO add CSS and format
             <div>
-                <form className = "search" action="google api placeholder">
-                    <input type ="text"
-                           className= "input"
-                           placeholder= "Search Recipes"/>
-                    <button type = "submit"> Search </button>
+                <form className="search" action="google api placeholder">
+                    <input type="text"
+                           className="input"
+                           placeholder="Search Recipes"/>
+                    <button type="submit"> Search</button>
                 </form>
             </div>
-
 
         );
     }
 }
-
 
 export default SearchBarComponent;
