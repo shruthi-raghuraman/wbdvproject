7b7cca0fb896457dbe7f287a7dfdf8c83b8043ed
shruthi-raghuraman <raghuraman.s@husky.neu.edu>
2020-03-25 14:29:22 -0400

    Adding google vision api

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const fileUpload = require('express-fileupload');
 
 const app = express();
 
+//Import google cloud client library
+const vision = require('@google-cloud/vision');
+
+//Create client
+const client = new vision.ImageAnnotatorClient({
+                                                   keyFilename: 'key.json'
+                                               });
+
+
 app.use(fileUpload());
 
 // Upload Endpoint
@@ -14,14 +23,29 @@ app.post('/upload', (req, res) => {
 
     const file = req.files.file;
 
-    file.mv(`${__dirname}/wbdv-sp20-group2-client-react/public/uploads/${file.name}`, err => {
+
+    file.mv(`${__dirname}/resources/${file.name}`, err => {
         if (err) {
             console.error(err);
             return res.status(500).send(err);
         }
+        client
+            .labelDetection('/Users/shruthiraghuraman/Documents/wbdv_project/wbdv-sp20-group2-project/resources/surprisefood.jpg')
+            .then(results => {
+                const labels = results[0].labelAnnotations;
 
-        res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+                console.log('Labels:');
+                labels.forEach(label => console.log(label));
+                res.json({ fileName: file.name,
+                             filePath: `/uploads/${file.name}`});
+            })
+            .catch(err => {
+                console.error('ERROR:', err);
+            });
     });
+
+
+
 });
 
 app.listen(5000, () => console.log('Server Started...'));
