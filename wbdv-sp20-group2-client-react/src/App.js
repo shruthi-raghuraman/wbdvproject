bef2498b9a240c44b6cb37a9ab47528b86ba3c4c
ffeghali <feghali.fa@husky.neu.edu>
2020-03-25 12:09:16 -0400

    file upload functionality, img won't show up yet, need to debug

--- wbdv-sp20-group2-client-react/src/App.js
+++ wbdv-sp20-group2-client-react/src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import FileUpload from "./components/FileUpload";
+import FileUpload from './components/FileUpload';
 import './App.css';
 
 const App = () => (
+    <div className='container mt-4'>
+        <h4 className='display-4 text-center mt-4'>
+            <i className='fas fa-cookie-bite' /> Recipes Finder
+        </h4>
 
-      <div className='container mt-4'>
-          <h4 className='display-4 text-center mb-4'>
-              <i className='fas fa-cookie-bite'></i> Recipes Finder
-          </h4>
-        <FileUpload/>
-      </div>
-
-  );
-
+        <FileUpload />
+    </div>
+);
 
 export default App;
